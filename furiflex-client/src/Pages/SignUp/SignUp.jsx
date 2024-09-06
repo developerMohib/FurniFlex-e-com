9fce6578a0277ca1a34da24070cc9cee4ad0c9ae
Mohib <mohibullahmohim2020@gmail.com>
2024-09-07 04:27:41 +0600

    sign up okay

--- furiflex-client/src/Pages/SignUp/SignUp.jsx
+++ furiflex-client/src/Pages/SignUp/SignUp.jsx
@@ -5,15 +5,41 @@ import apple from "../../assets/apple.png";
 import google from "../../assets/google.png";
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import bgImg from "../../assets/user-enter.png";
+import useAuth from "../../Hooks/useAuth";
+import Swal from 'sweetalert2'
 
 const SignUp = () => {
   const [showPass, setShowPass] = useState(false);
+  const { createUser } = useAuth();
+
+const handleSubmit = async (e) =>{
+  e.preventDefault();
+  const form = e.target;
+  
+  // Collecting values from the form
+  const name = form.firstName.value;
+  const email = form.email.value;
+  const password = form.password.value;
+  console.log('from', name, email,password );
+  form.reset();
+
+  // create user
+  await createUser(email, password);
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Your work has been saved",
+    showConfirmButton: false,
+    timer: 1500
+  });
+}
+
   return (
     <div>
       <div className="h-screen flex">
         <div className="flex w-full lg:w-1/2 justify-center items-center space-y-8">
           <div className="w-full px-8 md:px-32 lg:px-24 ">
-            <form className="bg-gray-100 rounded-md p-5">
+            <form onSubmit={handleSubmit} className="bg-gray-100 rounded-md p-5">
               <div className="text-center">
                 <h1 className="text-gray-800 font-semibold text-xl mb-1">
                   Welcome to
@@ -73,6 +99,7 @@ const SignUp = () => {
                     className="peer h-full w-full rounded-md border border-blue-gray-200 px-3 py-3 text-sm font-normal text-blue-gray-700 outline-none transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-1 focus:border-secondary focus:border-t-transparent focus:outline-none disabled:border-0 disabled:bg-blue-gray-50"
                     placeholder=" "
                     required
+                    name="password"
                   />
                   <label className="before:content-[' '] after:content-[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-secondary peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:!border-secondary peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                     Password
